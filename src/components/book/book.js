d07e9313cbe28b4fd6f761a0df6097d864a08a60
yonasketema <yonask.kitaw@gmail.com>
2022-06-21 10:39:01 -0700

    emotion book

--- src/components/book/book.js
+++ src/components/book/book.js
@@ -1,93 +1,32 @@
 import React, { useState } from "react";
-import Comment from "./../../containers/comment/comment";
-import Classes from "./book.module.css";
 
-const Book = ({
-  imgAddress,
-  title,
-  likes,
-  id,
-  likePost,
-  liked,
-  dislike,
-  saved,
-  unsaved,
-  savedBook,
-  getBookReview,
-  bookReviews,
-  setBookReviews,
-}) => {
-  const [isOpen, setIsOpen] = useState(false);
-
+const Book = () => {
   return (
-    <section className={Classes.book_container}>
-      <img src={imgAddress} alt="" />
-      <div className={Classes.img_tag}>
-        <p>{title}</p>
+    <section>
+      <img src="" alt="" />
+      <div>
+        <p>yonpia</p>
 
-        <div className={Classes.review_section}>
-          <div className={Classes.like_comment}>
-            {liked ? (
-              <button className={Classes.btn_saved}>
-                <i
-                  className=" heart fa-solid fa-heart fa-2x "
-                  onClick={() => dislike(id)}
-                  style={{ color: "red" }}
-                ></i>
-
-                {likes}
-              </button>
-            ) : (
-              <button className={Classes.btn_saved}>
-                <i
-                  className=" heart fa-solid fa-heart fa-2x "
-                  onClick={() => likePost(id)}
-                  style={{ color: "#ccc" }}
-                ></i>
-
-                {likes}
-              </button>
-            )}
+        <div>
+          <div>
+            <button>
+              <i className=" heart fa-solid fa-heart fa-2x "></i>
+            </button>
 
             <div>
-              <button
-                className={Classes.btn_saved}
-                onClick={(e) => {
-                  setIsOpen(true);
-                  getBookReview(id);
-                }}
-              >
+              <button>
                 <i className="fa-solid fa-comment fa-2x"></i>
               </button>
-              <Comment
-                img={imgAddress}
-                bookReviews={bookReviews}
-                setBookReviews={setBookReviews}
-                bookid={id}
-                open={isOpen}
-                onClose={() => setIsOpen(false)}
-              />
             </div>
           </div>
 
-          <div className={Classes.save_container}>
-            {savedBook ? (
-              <button className={Classes.btn_saved}>
-                <i
-                  className="fa-solid fa-bookmark fa-2x"
-                  onClick={() => unsaved(id)}
-                  style={{ color: "#292929" }}
-                ></i>
-              </button>
-            ) : (
-              <button className={Classes.btn_saved}>
-                <i
-                  className="fa-solid fa-bookmark fa-2x"
-                  onClick={() => saved(id)}
-                  style={{ color: "#ccc" }}
-                ></i>
-              </button>
-            )}
+          <div>
+            <button>
+              <i className="fa-solid fa-bookmark fa-2x"></i>
+            </button>
+            <button>
+              <i className="fa-solid fa-bookmark fa-2x"></i>
+            </button>
           </div>
         </div>
       </div>
